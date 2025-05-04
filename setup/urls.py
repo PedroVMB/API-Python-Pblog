32d3507b68d19aebb760e4417b9d236f0822fb6b
PedroVMB <pedromb.dev@gmail.com>
2025-05-04 18:29:02 -0300

    feat: primeiro commit

---
+++ setup/urls.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentação da API",
+      default_version='v1',
+      description="Documentação da API pblog",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="pedromb.dev@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
+router = routers.DefaultRouter()
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
