9e95fcc3b52f131938bceb5a6e9e5263deddcbf0
PedroVMB <pedromb.dev@gmail.com>
2025-05-04 18:59:06 -0300

    Feat: adicionando o prefixo api na chamada do swagger e do redoc

--- setup/urls.py
+++ setup/urls.py
@@ -27,8 +27,8 @@ router.register('posts', PostViewSet, basename='Posts')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
